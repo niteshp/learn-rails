c4fc39853abce54a32073e019dfb761758624f3d
niteshp <patel.nitesh@gmail.com>
2013-09-08 15:12:18 -0700

    sending mail ability added

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,8 +3,8 @@ class VisitorsController < ApplicationController
 	def new
 		@owner = Owner.new
 		# render 'visitors/new'	# Optional
-		flash[:notice] = 'Welcome!'
-		flash[:alert] = 'My birthday is soon.'
+		# flash[:notice] = 'Welcome!'
+		# flash[:alert] = 'My birthday is soon.'
 	end
 
 end
